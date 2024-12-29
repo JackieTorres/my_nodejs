06adc2f53e93c100cb1a3b431beecf321872fa5c
torres <jie11_11@163.com>
2024-12-29 20:52:14 +0800

    用formidable组件保存上传的图片

--- sggNodejs/08gen/routes/index.js
+++ sggNodejs/08gen/routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var { formidable } = require("formidable");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/portrait", (req, res) => {
+  res.render("portrait.ejs");
+});
+router.post("/portrait", (req, res) => {
+  const form = formidable({
+    multiples: true,
+    uploadDir: __dirname + "/../public/images",
+    keepExtensions: true,
+  });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // res.json({ fields, files });
+    // console.log(fields);
+    // console.log(files);
+    res.send({ fields, files });
+  });
 });
 
 module.exports = router;
