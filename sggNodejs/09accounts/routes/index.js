d6cbbca1dc696ebc0d81485a988fbe62d6fc1a35
torres <jie11_11@163.com>
2025-01-02 23:19:10 +0800

    ti

--- sggNodejs/09accounts/routes/index.js
+++ sggNodejs/09accounts/routes/index.js
@@ -1,5 +1,10 @@
 var express = require("express");
 var router = express.Router();
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync(__dirname + "/../data/db.json");
+const db = low(adapter);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
